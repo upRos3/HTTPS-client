15cb6763b4baabfadae0c2029338e572f10bfba4
J Rose <josh@upros3.com>
2018-01-22 18:51:30 +0000

    callback made

---
+++ getHTML.js
@@ -0,0 +1,30 @@
+var https = require('https');
+
+function getHTML (options, callback) {
+
+  var buffer = "";
+
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      buffer += data;
+    });
+
+    response.on('end', function() {
+       callback(buffer);
+    });
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
